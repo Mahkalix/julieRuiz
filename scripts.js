b8f68d0901bd8c6a19d3240da2f0de0f8cc3ffec
Maxence Badin Léger <maxencelger@gmail.com>
2024-12-29 19:14:48 +0100

    favico

--- scripts.js
+++ scripts.js
@@ -11,14 +11,13 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Slider functionality
     let slideIndex = 0;
+    let slideTimeout;
     showSlides();
 
-    window.plusSlides = function(n) {
-        showSlides(slideIndex += n);
-    }
-
     window.currentSlide = function(n) {
-        showSlides(slideIndex = n - 1);
+        clearTimeout(slideTimeout);
+        slideIndex = n - 1;
+        showSlides();
     }
 
     function showSlides() {
@@ -29,14 +28,18 @@ document.addEventListener("DOMContentLoaded", function () {
             slides[i].classList.remove("active");
             slides[i].classList.add("inactive");
         }
-        slideIndex++;
-        if (slideIndex > slides.length) {slideIndex = 1}
+        if (slideIndex >= slides.length) {
+            slideIndex = 0;
+        } else if (slideIndex < 0) {
+            slideIndex = slides.length - 1;
+        }
         for (i = 0; i < dots.length; i++) {
             dots[i].className = dots[i].className.replace(" hero-active", "");
         }
-        slides[slideIndex-1].classList.add("active");
-        slides[slideIndex-1].classList.remove("inactive");
-        dots[slideIndex-1].className += " hero-active";
-        setTimeout(showSlides, 4000); // Change slide every 4 seconds
+        slides[slideIndex].classList.add("active");
+        slides[slideIndex].classList.remove("inactive");
+        dots[slideIndex].className += " hero-active";
+        slideIndex++;
+        slideTimeout = setTimeout(showSlides, 4000); // Change slide every 4 seconds
     }
 });
